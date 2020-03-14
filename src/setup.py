d1f3fd39b6389a9aee3cf59459a91dd20d693642
jarulsamy <joshua.gf.arul@gmail.com>
2020-03-13 23:05:26 -0600

    Fixed setup classifiers

--- src/setup.py
+++ src/setup.py
@@ -12,8 +12,15 @@ setup(
     description="A robot communication library for Scribbler / Scribbler 2",
     url="https://github.com/jarulsamy/myro",
     classifiers=[
+        # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+        "Development Status :: 4 - Beta",
+        # Define that your audience are developers
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        # Specify which pyhton versions that you want to support
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
-        "Licence :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
     ],
 )
