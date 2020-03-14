a3c603ffe9940a7e4e2eeaa67534947a950091b4
jarulsamy <joshua.gf.arul@gmail.com>
2020-03-13 22:51:52 -0600

    Fixed setup script.

--- src/setup.py
+++ src/setup.py
@@ -1,25 +1,19 @@
 # -*- coding: utf-8 -*-
-import setuptools
+from setuptools import find_packages
+from setuptools import setup
 
-with open("myro/README.md", "r") as fh:
-    long_description = fh.read()
-
-
-setuptools.setup(
+setup(
     name="myro",
     version="3.0.0",
-    scripts=["myro/"],
+    packages=find_packages(),
+    install_requires=["pyserial"],
     author="Joshua Arulsamy",
     author_email="joshua.gf.arul@gmail.com",
     description="A robot communication library for Scribbler / Scribbler 2",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
     url="https://github.com/jarulsamy/myro",
-    packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3.7",
         "Licence :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=["pyserial"],
 )
