73bb61f8eab5a11124b440d3f3ded56e448e7366
Joshua Arulsamy <joshua.gf.arul@gmail.com>
2020-07-08 13:45:41 -0600

    :bug: Fixed broken import to version.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages
 from setuptools import setup
 
-from myro.version import __VERSION__
+from myro.__version__ import __VERSION__
 
 setup(
     name="myro",
@@ -13,13 +13,13 @@ setup(
     description="A robot communication library for Scribbler / Scribbler 2",
     url="https://github.com/jarulsamy/myro",
     classifiers=[
-        # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+        # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable"
+        # as the current state of your package
         "Development Status :: 4 - Beta",
         # Define that your audience are developers
         "Intended Audience :: Developers",
         "Topic :: Software Development :: Build Tools",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-        # Specify which pyhton versions that you want to support
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
